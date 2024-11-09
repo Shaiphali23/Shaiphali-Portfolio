8c864dd1275b64db09492c57a721e54ef14361dd
Shaiphali Jaiswal <142350579+Shaiphali23@users.noreply.github.com>
2024-11-09 14:58:04 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,10 @@ const PORT = process.env.PORT || 5000;
 
 //Middleware
 app.use(bodyParser.json());
-app.use(cors());
+app.use(cors({
+    origin: ["https://shaiphali-portfolio-lis36ppah-shaiphali-jaiswals-projects.vercel.app", "http://localhost:3000"],
+    credentials: true,
+  }));
 
 //Routes
 const contactRoutes = require("./routes/contactRoutes");
