9a7f901a0e3ab41869ce78fa6f7db7ec496de1ca
Shaiphali Jaiswal <142350579+Shaiphali23@users.noreply.github.com>
2024-11-13 20:45:12 +0530

    Update script.js

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -61,7 +61,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
     try {
       //send post request to backend
-      const response = await fetch("", {
+      const response = await fetch("https://shaiphali-portfolio.onrender.com/api/contact", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
